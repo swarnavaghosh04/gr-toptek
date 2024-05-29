b922f8f8a4c0f85bcac478d9099b8fe88850f302
Swarnava Ghosh <49722007+swarnavaghosh04@users.noreply.github.com>
2024-05-29 16:10:42 -0400

    updated comments

--- python/toptek/PTT.py
+++ python/toptek/PTT.py
@@ -32,7 +32,7 @@ import sys
 
 class PTT(gr.sync_block):
     """
-    docstring for block PTT
+    enable Tx ampilifier for the duration of transmission basaed on packet length
     """
     def __init__(self, baud_rate=9600, padding_nbytes=0, delay_s=0, extra_s=0):
         gr.sync_block.__init__(self,
@@ -44,8 +44,6 @@ class PTT(gr.sync_block):
         self.delay = delay_s
         self.extra = extra_s
         self.message_port_register_in(pmt.intern('tx_msg'))
-        self.message_port_register_out(pmt.intern('tx_amp'))
-        self.message_port_register_out(pmt.intern('tx_vga_gain'))
         self.message_port_register_out(pmt.intern('pa'))
         self.message_port_register_out(pmt.intern('da'))
         self.message_port_register_out(pmt.intern('lna'))
@@ -63,27 +61,13 @@ class PTT(gr.sync_block):
         length = (len(msg)+self.padding_nbytes)*8
         tx_time = length/self.baud_rate + self.extra
         time.sleep(self.delay)
-        # self.amp.da_on()
         self.message_port_pub(
             pmt.intern('da'),
             pmt.cons(pmt.PMT_NIL, pmt.PMT_T))
-        # self.message_port_pub(
-        #    pmt.intern("tx_amp"),
-        #    pmt.cons(pmt.intern("tx_amp"), pmt.PMT_T))
-        # self.message_port_pub(
-        #    pmt.intern("tx_vga_gain"),
-        #    pmt.cons(pmt.intern("tx_vga_gain"), pmt.to_pmt(47)))
         time.sleep(tx_time)
         self.message_port_pub(
             pmt.intern('da'),
             pmt.cons(pmt.PMT_NIL, pmt.PMT_F))
-        # self.amp.da_off()
-        #self.message_port_pub(
-        #    pmt.intern("tx_amp"),
-        #    pmt.cons(pmt.intern("tx_amp"), pmt.PMT_F))
-        #self.message_port_pub(
-        #    pmt.intern("tx_vga_gain"),
-        #    pmt.cons(pmt.intern("tx_vga_gain"), pmt.to_pmt(0)))
 
     def work(self, input_items, output_items):
         return 0
